0a667b2fab94c3bca1273bb7449624e1fd0ace3f
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-17 00:18:34 +0530

    feat: update Hero component with GSAP animations and adjust hero heading font size for responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+
+import {ScrollTrigger} from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -22,41 +26,70 @@ const Hero = () => {
   const handleVideoload = () => {
     setLoadedVideos((prev) => prev + 1);
   };
-
-  useGSAP(()=>{
-    if(hasClicked){
-        gsap.set("#next-video",{visibility:'visible'})
-        gsap.to('#next-video',{
-            transformorigin:'center center',
-            scale:1,
-            width:'100%',
-            height:'100%',
-            duration:1,
-            ease:'power1.inOut',
-            onStart:() => nextVideoRef.current.play()
-        })
-        gsap.from('#current-video',{
-            transformOrigin:'center center',
-            scale:0,
-            duration:1.5,
-            ease:'power1.inOut',
-            onStart:()=>nextVideoRef.current.play()
-        })
+  useEffect(()=>{
+    if(loadedVideos === totalVideos -1){
+      setIsLoading(false)
     }
-  },{dependencies:[currentIndex],revertOnUpdate:true})
+  },[loadedVideos])
 
-  useGSAP(()=>{
-    gsap.set('#video-frame',{
-        clipPath:
-    })
-  })
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+      }
+    },
+    { dependencies: [currentIndex], revertOnUpdate: true }
+  );
+
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)",
+      borderRadius: "0 0 40% 10%",
+    });
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%,100% 0%,100% 100%, 0% 100%)",
+      borderRadius:'0 0 0 0',
+      ease:'power1.inOut',
+      scrollTrigger:{
+        trigger:'#video-frame',
+        start:'center center',
+        end:'bottom center',
+        scrub: true,
+      }
+    });
+  });
 
   const getVideoSrc = (index) => `../../public/videos/hero-${index}.mp4`;
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
+      {isLoading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+          </div>
+        </div>
+      )}
       <div
         id="video-frame"
-        className="relative z-10 h-dvh w-screen overflow-x-hidden rounded-lg bg-blue-75"
+        className="relative z-10 h-dvh overflow-y-hidden w-screen overflow-x-hidden rounded-lg bg-blue-75"
       >
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
@@ -81,7 +114,7 @@ const Hero = () => {
             loop
             muted
             id="next-video"
-            className="absolute-center invisible absolute z-20 s-ze-64 object-cover object-center"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
             onLoadedData={handleVideoload}
           />
           <video
@@ -115,10 +148,11 @@ const Hero = () => {
             />
           </div>
         </div>
+
       </div>
-      <h1 className="special-font hero-heading absolute bottom-5 right-5  text-black">
-        G<b>a</b>ming
-      </h1>
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+          G<b>a</b>ming
+        </h1>
     </div>
   );
 };
