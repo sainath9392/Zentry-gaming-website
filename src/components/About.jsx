5ac0311f23986eafc63bf472ce38c2e3b9252208
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-17 22:07:23 +0530

    feat: integrate About component into App and remove unused section

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about' className='min-h-screen w-screen bg-blue-50'>
+        <div className='relative mb-8 mt-36 flex flex-col'>
+
+        </div>
+    </div>
+  )
+}
+
+export default About
