ce05e8d97dc2d0d7c0510b534111efc2f42d04a4
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-18 23:49:35 +0530

    feat: add Features component to App and improve text styling in About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -36,7 +36,7 @@ const About = () => {
         />
         <div className="about-subtext">
           <p>The Game of Games begins-your life, now an epic MMORPG</p>
-          <p>Zentry unites every player from countless games and platforms</p>
+          <p className="text-gray-500">Zentry unites every player from countless games and platforms</p>
         </div>
       </div>
       <div className="h-dvh w-screen" id="clip">
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Button = ({ title, id, leftIcon, containerClass ,rightIcon }) => {
-  return <button className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>{leftIcon} <span className="relative incline-flex overflow-hidden font-general text-xs uppercase">
-    <div>{title}</div>
+  return <button className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3  text-black ${containerClass}`}>{leftIcon} <span className="relative incline-flex overflow-hidden font-general text-xs uppercase">
+    <div >{title}</div>
     </span>
     {rightIcon}</button>;
 };
