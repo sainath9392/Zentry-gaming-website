453ac27ca3fe9f6af605380dae445231d8667071
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-19 23:02:30 +0530

    feat: enhance BentoCard component with video playback on hover and improve layout in Features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,14 +1,40 @@
-const BentoCard = ({ src, title, description, isComingSoon }) => {
+import React, { useRef, useState } from "react";
+
+const BentoCard = ({
+  src,
+  title,
+  description,
+  conatinerClass,
+  isComingSoon,
+}) => {
+  const videoRef = useRef(null);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+    videoRef.current.play();
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+    videoRef.current.pause();
+    videoRef.current.currentTime = 0;
+  };
+
   return (
-    <div className="relative size-full">
+    <div
+      className="relative size-full cursor-pointer"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <video
+        ref={videoRef}
         src={src}
         loop
         muted
-        autoPlay
-        className="absolute left-0 top-0 size-full object-cover object-center"
+        className={`absolute left-0 top-0 size-full border-2 border-gray-700 ${conatinerClass}`}
       />
-      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50 max-sm:w-40 ">
         <div>
           <h1 className="bento-title special-font ">{title}</h1>
           {description && (
@@ -24,7 +50,7 @@ const Features = () => {
   return (
     <section className="bg-black pb-52">
       <div className="container mx-auto px-3 md:px-10">
-        <div className="px-5 py-32">
+        <div className="px-5 sm:px-30 py-32">
           <p className="font-circular-web text-lg text-blue-50">
             Into the Metagame Layer
           </p>
@@ -35,7 +61,7 @@ const Features = () => {
           </p>
         </div>
 
-        <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+        <div className="border-hsla md:h-[40vh] max-lg:h-[75vh] relative mb-7 h-96  overflow-hidden rounded-md lg:mx-30">
           <BentoCard
             src="videos/feature-1.mp4"
             title={
@@ -43,9 +69,49 @@ const Features = () => {
                 radia<b>n</b>t
               </>
             }
+            conatinerClass={"object-cover object-center"}
             description="A cross-platform metagame app, turning your ideas activities across web2 and web3 games into a rewarding adventure."
             isComingSoon
           />
+        </div>
+
+        <div className="grid h-[90vh] lg:h-[135vh] xl:h-[165vh] grid-cols-2 grid-rows-3 gap-7 lg:mx-20">
+          <div className="bento-tilt_1 row-span-1 col-span-2 max-sm:h-[45vh] md:col-span-1 md:row-span-2">
+            <BentoCard
+              conatinerClass={"object-cover md:object-fill max-sm:h-[45vh] max-sm:w-[70vw] "}
+              src="videos/feature-2.mp4"
+              title={
+                <>
+                  zig<b>m</b>a
+                </>
+              }
+              description="An anime and gaming-inspired NFT collection - the Ip primed for expansion."
+            />
+          </div>
+          <div className="bento-tilt_1 max-sm:absolute max-sm:left-37 max-sm:top-34  row-span-1 col-span-2 md:col-span-1 ">
+            <BentoCard
+              src="videos/feature-3.mp4"
+              conatinerClass={" object-cover md:object-contain md:object-right  max-sm:h-[25vh] max-sm:w-[55vw]"}
+              title={
+                <>
+                  n<b>e</b>xus
+                </>
+              }
+              description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities"
+            />
+          </div>
+          <div className="bento-tilt_1 max-sm:absolute max-sm:right-20 max-sm:top-26 row-span-1 col-span-2 ms-22 md:col-span-1 md:ms-0">
+            <BentoCard
+            conatinerClass={"object-cover max-sm:h-[25vh] max-sm:w-[70vw]"}
+              src="videos/feature-4.mp4"
+              title={
+                <>
+                  az<b>u</b>l
+                </>
+              }
+              description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities"
+            />
+          </div>
         </div>
       </div>
     </section>
