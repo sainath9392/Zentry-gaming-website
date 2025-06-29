2a766f5316e865b3e656f6df77de816bcf0da879
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-29 22:05:23 +0530

    fix: update Specs component to ensure consistent text color and styling

--- src/components/Specs.jsx
+++ src/components/Specs.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Specs = ({ title, no, context, containerClass }) => {
   return (
-    <div className={`font-circular-web ${containerClass} `}>
-      <div className="flex gap-4 font-general font-bold text-xs uppercase md:text-[10px] text-black">
+    <div className={`font-circular-web text-black ${containerClass} `}>
+      <div className="flex gap-4 font-general font-bold text-xs uppercase md:text-[10px] ">
         <p>{no}</p>
         <h6>{title}</h6>
       </div>
-      <div className="flex gap-7 font-general text-[8px] uppercase md:text-[10px] mt-1 text-black">
+      <div className="flex gap-7 font-general text-[8px] uppercase md:text-[10px] mt-1 ">
         <div></div>
         <p>{context}</p>
       </div>
