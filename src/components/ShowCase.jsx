c4fdd17cf272d91a3906d6409482a81ea720a531
sainath9392 <sainathduvvuri03@gmail.com>
2025-07-01 23:39:03 +0530

    feat: implement scroll-triggered background color changes in ShowCase component

--- src/components/ShowCase.jsx
+++ src/components/ShowCase.jsx
@@ -1,12 +1,13 @@
 import React, { useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import AnimatedTitle from "./AnimatedTitle";
 
 gsap.registerPlugin(ScrollTrigger);
 const ShowCase = () => {
   const sectionsRef = useRef([]);
 
-  useLayoutEffect(()=>{
+  useLayoutEffect(() => {
     const section1 = sectionsRef.current[0];
     const color = section1.getAttribute("data-color");
 
@@ -42,13 +43,23 @@ const ShowCase = () => {
     return () => {
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
-  },[])
+  }, []);
   return (
     <section
       ref={(el) => (sectionsRef.current[0] = el)}
       data-color="#000000"
-      className="min-h-dvh overflow-y-hidden w-screen sm:pt-45"
-    ></section>
+      className="min-h-dvh overflow-y-hidden w-screen sm:pt-45 relative"
+    >
+      <div className="flex md:absolute md:left-2 flex-col items-center justify-center w-full">
+        <AnimatedTitle
+          title={"Ze<b>n</b>try at a <br />glan<b>c</b>e"}
+          containerClass="animated-title  w-full mt-40 md:mt-20 z-10"
+          color={
+            "flex justify-center h-[50px] md:h-[80px] md:px-2 sm:text-6xl ani text-5xl md:text-8xl "
+          }
+        />
+      </div>
+    </section>
   );
 };
 
