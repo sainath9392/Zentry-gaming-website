38b4a9234f7dcf6019278297083bf8a116c49e13
sainath9392 <sainathduvvuri03@gmail.com>
2025-07-02 19:58:25 +0530

    Implement code changes to enhance functionality and improve performance

--- src/components/ShowCase.jsx
+++ src/components/ShowCase.jsx
@@ -6,7 +6,6 @@ import RoundedCorners from "./RoundedCorners";
 
 gsap.registerPlugin(ScrollTrigger);
 const ShowCase = () => {
-
   const sectionsRef = useRef([]);
 
   useLayoutEffect(() => {
@@ -58,62 +57,115 @@ const ShowCase = () => {
           duration: 0,
         });
       },
-      
     });
     return () => {
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
   }, []);
 
-
-
   return (
     <section
       ref={(el) => (sectionsRef.current[0] = el)}
       data-color="#000000"
-      className="min-h-dvh overflow-x-hidden w-screen relative"
+      className="min-h-[300vh] overflow-hidden w-screen relative"
     >
-      <div className="min-h-dvh">
-        <div className="flex flex-col items-center sm:items-start justify-center sm:justify-start w-full sm:ml-20 mt-52">
-          <p className="font-general h-[10px] font-medium ani text-xs uppercase md:text-[8px] sm:ml-4">
-            who we are
-          </p>
-          <AnimatedTitle
-            title={"Ze<n>try at a <br />glan<c>e"}
-            containerClass="animated-title w-full mt-5 sm:mt-2 z-10"
-            color="flex justify-center sm:justify-start md:px-2 text-5xl sm:text-6xl md:text-[90px] ani"
-          />
-        </div>
+      <div className="flex flex-col items-center sm:items-start justify-center sm:justify-start w-full sm:ml-20 mt-52">
+        <p className="font-general h-[10px] font-medium ani text-xs uppercase md:text-[8px] sm:ml-4">
+          who we are
+        </p>
+        <AnimatedTitle
+          title={"Ze<n>try at a <br />glan<c>e"}
+          containerClass="animated-title w-full mt-5 sm:mt-2 z-10"
+          color="flex justify-center sm:justify-start md:px-2 text-5xl sm:text-6xl md:text-[90px] ani"
+        />
+      </div>
 
-        <div className="flex flex-col w-full h-full mt-52 relative">
-          <div className="flex justify-between h-[200px] w-[340px] border absolute right-5 border-gray-900 rounded">
-            <div className="flex flex-col w-[4px] items-start justify-start mt-2 ml-2">
-              <p className="font-general font-medium ani text-xs uppercase md:text-[8px]">
-                products
-              </p>
-              <AnimatedTitle
-                title={"4<b>+</b>"}
-                containerClass="animated-title z-10 w-full -ml-10"
-                color="flex justify-start text-7xl ani"
-              />
-            </div>
-            <video
-              className="w-fit object-cover"
-              autoPlay
-              loop
-              muted
-              src="/videos/case1.webm"
+      <div className="flex flex-col w-full mt-52 relative">
+        <div className="flex justify-between h-[200px] w-[340px] border absolute right-5 border-gray-900 rounded">
+          <div className="flex flex-col w-[4px] items-start justify-start mt-2 ml-2">
+            <p className="font-general font-medium ani text-xs uppercase md:text-[8px]">
+              products
+            </p>
+            <AnimatedTitle
+              title={"4<b>+</b>"}
+              containerClass="animated-title z-10 w-full -ml-10"
+              color="flex justify-start text-7xl ani"
             />
           </div>
-        </div>
-       
-        <div className="mt-56 relative h-[200px] overflow-hidden">
-          <div className="absolute h-[200px] right-5 w-[200px] bg-[#EDFF66] rounded z-10">
-            <p className="font-sans  special-font text-9xl ml-2"><b>30+</b></p>
-          </div>
-          <p className="font-circular-web text-xs font-semibold z-10 absolute bottom-4 right-10">Partners</p>
+          <video
+            className="w-fit object-cover"
+            autoPlay
+            loop
+            muted
+            src="/videos/case1.webm"
+          />
         </div>
       </div>
+
+      <div className=" relative h-[200px] mt-56 overflow-hidden">
+        <div className="absolute h-[200px] right-5 w-[200px] bg-[#EDFF66] rounded z-10">
+          <p className="font-sans  special-font text-9xl ml-2">
+            <b>30+</b>
+          </p>
+        </div>
+        <p className="font-circular-web text-xs font-semibold z-10 absolute bottom-4 right-10">
+          Partners
+        </p>
+      </div>
+
+      <div className="absolute mt-6 h-[380px] w-[280px] bg-[#5542FF] rounded-lg right-20 z-10">
+        <div className="flex flex-col w-[4px] items-start justify-start mt-2 ml-2">
+          <p className="font-circular-web font-medium text-black text-[10px] uppercase ml-3">
+            Tresury
+          </p>
+          <p className="font-sans  special-font text-6xl ml-2">
+            <b>150M+</b>
+          </p>
+        </div>
+        <video
+          className="w-fit -mt-24 h-[382px]"
+          autoPlay
+          loop
+          muted
+          src="/videos/case3.webm"
+        />
+        <div className="flex justify-around items-center -mt-8">
+          <div className="flex justify-between items-center gap-2">
+            <div className="h-2 w-2 rounded-full bg-black" />
+            <div className="text-[7px] text-blue-100">
+              <p>LIQUID TOKEN</p>
+              <p>78%</p>
+            </div>
+          </div>
+          <div className="flex justify-between items-center gap-2">
+            <div className="h-2 w-2 rounded-full bg-[#EDFF66]" />
+            <div className="text-[7px] text-blue-100">
+              <p>INVESTMENTS</p>
+              <p>20%</p>
+            </div>
+          </div>
+          <div className="flex justify-between items-center gap-2">
+            <div className="h-2 w-2 rounded-full bg-blue-100" />
+            <div className="text-[7px] text-blue-100">
+              <p>NFT ASSETS</p>
+              <p>10%</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute mt-108 h-[379px] w-[280px] bg-[#5542FF] rounded-lg left-5 z-10">
+        <div className="flex flex-col w-[4px] items-start justify-start mt-2 ml-2">
+          <p className="font-circular-web font-medium text-black text-[10px] uppercase ml-3">
+            Residents
+          </p>
+          <p className="font-sans  special-font text-8xl ml-2">
+            <b>500k+</b>
+          </p>
+        </div>
+        <img className="absolute top-0 z-10" src="/img/case4.webp" alt="" />
+      </div>
+      
     </section>
   );
 };
