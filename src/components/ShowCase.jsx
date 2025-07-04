30223b11dc8a981ad0ab963ded08d4c630c5a88f
sainath9392 <sainathduvvuri03@gmail.com>
2025-07-04 22:18:38 +0530

    feat: improve layout and styling of ShowCase component for enhanced responsiveness and visual consistency

--- src/components/ShowCase.jsx
+++ src/components/ShowCase.jsx
@@ -79,121 +79,120 @@ const ShowCase = () => {
           color="flex justify-center sm:justify-start md:px-2 text-5xl sm:text-6xl md:text-[90px] ani"
         />
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2  mx-auto w-[50vw] h-[135vh] xl:h-[145vh] xl:mx-40 gap-7 mt-50">
-        
-        <div className="flex justify-between row-end-1 h-[200px] w-[340px] border -ms-8 col-span-2 md:col-span-1 border-gray-900 rounded">
-          <div className="flex flex-col w-[4px] items-start justify-start mt-2 ml-2">
-            <p className="font-general font-medium text-blue-50 text-xs uppercase md:text-[8px]">
-              products
-            </p>
-            <AnimatedTitle
-              title={"4<b>+</b>"}
-              containerClass="animated-title z-10 w-full -ml-10"
-              color="flex justify-start text-7xl ani"
+      <div className="container mx-[10px] w-full px-3 md:px-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2  mx-auto w-[50vw] h-[135vh] xl:h-[145vh] xl:mx-40 gap-7 mt-50">
+          <div className="flex justify-between row-end-1 sm: h-[200px] w-[340px] border -ms-16 sm:-ms-18  col-span-2 sm:col-span-1 border-gray-900 rounded">
+            <div className="flex flex-col w-[4px] items-start justify-start mt-2 ml-2">
+              <p className="font-general font-medium text-blue-50 text-xs uppercase md:text-[8px]">
+                products
+              </p>
+              <AnimatedTitle
+                title={"4<b>+</b>"}
+                containerClass="animated-title z-10 w-full -ml-10"
+                color="flex justify-start text-7xl ani"
+              />
+            </div>
+            <video
+              className="w-fit object-cover"
+              autoPlay
+              loop
+              muted
+              src="/videos/case1.webm"
             />
           </div>
-          <video
-            className="w-fit object-cover"
-            autoPlay
-            loop
-            muted
-            src="/videos/case1.webm"
-          />
-        </div>
 
-        <div className="relative col-span-2 md:col-span-1 -ms-25 row-end-4 h-[379px] w-[280px] bg-[#5542FF] rounded-lg  z-10">
-          <div className="flex flex-col w-[4px] items-start justify-start mt-2 ml-2">
-            <p className="font-circular-web font-medium text-black text-[10px] uppercase ml-3">
-              Residents
-            </p>
-            <p className="font-sans  special-font text-8xl ml-2">
-              <b>500k+</b>
-            </p>
-          </div>
-          <img className="absolute top-0 z-10" src="/img/case4.webp" alt="" />
-        </div>
-
-        <div className="relative col-span-2 ms-27 md:col-span-1 row-end-2  h-[200px] w-full  overflow-hidden">
-          <div className=" h-[200px]  w-[200px] bg-[#EDFF66] rounded z-10">
-            <p className="font-sans  special-font text-9xl ml-2">
-              <b>30+</b>
-            </p>
-          </div>
-          <p className="font-circular-web text-xs font-semibold z-10 absolute bottom-4 right-10">
-            Partners
-          </p>
-        </div>
-
-         <div className="relative h-60 w-60 ms-18 -mt-3 text-blue-50 row-end-5 border overflow-hidden border-gray-800 rounded-lg col-span-2 md:col-span-1">
-          <p className="font-sans special-font norm-heading text-4xl px-2 py-2 uppercase ml-2">
-            W<b>O</b>rld-class <br />b<b>a</b>ckers
-          </p>
-          <p className="font-general uppercase text-blue-50 -mt-0.4 text-[10px] absolute right-5">
-            Coinbase ventures <br />
-            yzi labs <br />
-            spartan <br />
-            longhash <br />
-            pantera capital <br />
-            animoca brands <br />
-            defiance capital <br />
-            play ventures <br />
-            skyvision capital <br />
-            vessel capital <br />
-          </p>
-        </div>
-
-        <div className="col-span-2 md:col-span-1 -ms-8 row-end-3 h-[380px] w-[290px] bg-[#5542FF] rounded-lg z-10">
-          <div className="flex flex-col w-[4px] items-start justify-start mt-2 ml-2">
-            <p className="font-circular-web font-medium text-black text-[10px] uppercase ml-3">
-              Tresury
-            </p>
-            <p className="font-sans  special-font text-6xl ml-2">
-              <b>150M+</b>
-            </p>
-          </div>
-          <video
-            className="w-fit -mt-24 h-[382px]"
-            autoPlay
-            loop
-            muted
-            src="/videos/case3.webm"
-          />
-          <div className="flex justify-around items-center -mt-8">
-            <div className="flex justify-between items-center gap-2">
-              <div className="h-2 w-2 rounded-full bg-black" />
-              <div className="text-[7px] text-blue-100">
-                <p>LIQUID TOKEN</p>
-                <p>78%</p>
-              </div>
+          <div className="relative col-span-2 md:col-span-1 -ms-22 -mt-2 row-end-4 sm:row-end-1 h-[379px] w-[280px] bg-[#5542FF] rounded-lg  z-10">
+            <div className="flex flex-col w-[4px] items-start justify-start mt-2 ml-2">
+              <p className="font-circular-web font-medium text-black text-[10px] uppercase ml-3">
+                Residents
+              </p>
+              <p className="font-sans  special-font text-8xl ml-2">
+                <b>500k+</b>
+              </p>
             </div>
-            <div className="flex justify-between items-center gap-2">
-              <div className="h-2 w-2 rounded-full bg-[#EDFF66]" />
-              <div className="text-[7px] text-blue-100">
-                <p>INVESTMENTS</p>
-                <p>20%</p>
-              </div>
+            <img className="absolute top-0 z-10" src="/img/case4.webp" alt="" />
+          </div>
+
+          <div className="relative col-span-2 ms-20 md:col-span-1 row-end-2 rounded-lg h-[200px] w-full  overflow-hidden">
+            <div className=" h-[200px]  w-[200px] bg-[#EDFF66] rounded z-10">
+              <p className="font-sans  special-font text-9xl ml-2">
+                <b>30+</b>
+              </p>
             </div>
-            <div className="flex justify-between items-center gap-2">
-              <div className="h-2 w-2 rounded-full bg-blue-100" />
-              <div className="text-[7px] text-blue-100">
-                <p>NFT ASSETS</p>
-                <p>10%</p>
+            <p className="font-circular-web text-xs font-semibold z-10 absolute bottom-4 right-10">
+              Partners
+            </p>
+          </div>
+
+          <div className="relative h-60 w-60 ms-5 -mt-3 text-blue-50 row-end-5 border overflow-hidden border-gray-800 rounded-lg col-span-2 md:col-span-1">
+            <p className="font-sans special-font norm-heading text-4xl px-2 py-2 uppercase ml-2">
+              W<b>O</b>rld-class <br />b<b>a</b>ckers
+            </p>
+            <p className="font-general uppercase text-blue-50 -mt-0.4 text-[10px] absolute right-5">
+              Coinbase ventures <br />
+              yzi labs <br />
+              spartan <br />
+              longhash <br />
+              pantera capital <br />
+              animoca brands <br />
+              defiance capital <br />
+              play ventures <br />
+              skyvision capital <br />
+              vessel capital <br />
+            </p>
+          </div>
+
+          <div className="col-span-2 md:col-span-1 -ms-10 row-end-3 h-[380px] w-[290px] -mt-2 bg-[#5542FF] rounded-lg z-10">
+            <div className="flex flex-col w-[4px] items-start justify-start mt-2 ml-2">
+              <p className="font-circular-web font-medium text-black text-[10px] uppercase ml-3">
+                Tresury
+              </p>
+              <p className="font-sans  special-font text-6xl ml-2">
+                <b>150M+</b>
+              </p>
+            </div>
+            <video
+              className="w-fit -mt-24 h-[382px]"
+              autoPlay
+              loop
+              muted
+              src="/videos/case3.webm"
+            />
+            <div className="flex justify-around items-center -mt-8">
+              <div className="flex justify-between items-center gap-2">
+                <div className="h-2 w-2 rounded-full bg-black" />
+                <div className="text-[7px] text-blue-100">
+                  <p>LIQUID TOKEN</p>
+                  <p>78%</p>
+                </div>
+              </div>
+              <div className="flex justify-between items-center gap-2">
+                <div className="h-2 w-2 rounded-full bg-[#EDFF66]" />
+                <div className="text-[7px] text-blue-100">
+                  <p>INVESTMENTS</p>
+                  <p>20%</p>
+                </div>
+              </div>
+              <div className="flex justify-between items-center gap-2">
+                <div className="h-2 w-2 rounded-full bg-blue-100" />
+                <div className="text-[7px] text-blue-100">
+                  <p>NFT ASSETS</p>
+                  <p>10%</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-       
-
-        <div className="relative -mt-3 -ms-25 bg-slate-300 row-end-6 h-[200px] w-[320px] rounded-lg col-span-2 md:col-span-1">
-          <div className="flex flex-col items-start justify-start mt-2 ml-2">
-            <p className="font-general font-medium text-black text-xs uppercase md:text-[8px]">
-              Revenue genrated <br />
-              2024
-            </p>
-            <p className="norm-heading absolute left-5 bottom-0 special-font text-9xl">
-              4<b>0M</b>
-            </p>
+          <div className="relative -mt-3 -ms-22 bg-slate-300 row-end-6 h-[200px] w-[320px] rounded-lg col-span-2 md:col-span-1">
+            <div className="flex flex-col items-start justify-start mt-2 ml-2">
+              <p className="font-general font-medium text-black text-xs uppercase md:text-[8px]">
+                Revenue genrated <br />
+                2024
+              </p>
+              <p className="norm-heading absolute left-5 bottom-0 special-font text-9xl">
+                4<b>0M</b>
+              </p>
+            </div>
           </div>
         </div>
       </div>
